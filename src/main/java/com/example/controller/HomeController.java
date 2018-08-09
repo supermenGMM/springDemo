e58f0ae604cf3b56dbadf3ba57e952da0db4906e
supermenG <852458550@qq.com>
2018-08-09 10:34:27 +0800

    change test

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -3,11 +3,21 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/home")
 public class HomeController {
     @RequestMapping("/hello")
     public String hello() {
+        System.out.println("变量var的值为" + 2 + ",当前类=class.method");
+        System.out.println("变量var的值为" + 9 + ",当前类=HomeController.hello");
+        List list = new ArrayList();
+        for (Object o : list) {
+            System.out.println("变量sfd的值为" + ",当前类=HomeController.hello");
+        }
+
         return "hello";
     }
 }
