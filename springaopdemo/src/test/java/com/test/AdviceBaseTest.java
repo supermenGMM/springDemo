a1123190e44c02f7fdc60c46276bb603511c9018
supermenG <852458550@qq.com>
2018-09-06 18:03:20 +0800

    spring aop

--- springaopdemo/src/test/java/com/test/AdviceBaseTest.java
+++ springaopdemo/src/test/java/com/test/AdviceBaseTest.java
@@ -1,7 +1,8 @@
 package com.test;
 
 import com.base.JunitTestBase;
-import com.mm.serivce.MovieService;
+import com.mm.introduction.Fit;
+import com.mm.serivce.impl.MovieService;
 import org.junit.Test;
 
 public class AdviceBaseTest extends JunitTestBase {
@@ -15,7 +16,22 @@ public class AdviceBaseTest extends JunitTestBase {
     @Test
     public void test() {
         MovieService myService = (MovieService) applicationContext.getBean("movieService");
-        myService.save();
+        myService.save(2);
+//        myService.play("aaa",2);
+
+        MovieService myService2 = (MovieService) applicationContext.getBean("movieService");
+        myService.save(3);
+    }
+
+    /**
+     * introductions
+     */
+    @Test
+    public void test2() {
+        Fit movieService = (Fit) applicationContext.getBean("movieService");
+        movieService.say();
+        Fit movieService2 = (Fit) applicationContext.getBean("movieService");
+        movieService2.say();
     }
 }
 
