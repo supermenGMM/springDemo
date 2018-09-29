b1970347830c43fd6252f3148231ded53be0dda3
supermenG <852458550@qq.com>
2018-09-29 18:20:58 +0800

    springbootmybatis  springboot 2.0.4 版本，更新，没有用mybatis官网 的方法

--- spring-boot-mybatis/src/main/java/com/mm/demo/dao/CityDao.java
+++ spring-boot-mybatis/src/main/java/com/mm/demo/dao/CityDao.java
@@ -1,11 +1,8 @@
 package com.mm.demo.dao;
 
 import com.mm.demo.domain.City;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-@Mapper
 public interface CityDao {
-//    @Select("select * from city where city_name =#{citname}")
-    City fingByName(@Param("cityName") String cityName);
+    City findByName(@Param("cityName") String cityName);
 }
